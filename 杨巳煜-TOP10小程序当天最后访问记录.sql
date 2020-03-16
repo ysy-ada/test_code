ccee46b3c88908c730dbe1fc9060ba269c8a3de1
ysy-ada <62235205+ysy-ada@users.noreply.github.com>
2020-03-16 16:18:10 +0800

    Update 杨巳煜-TOP10小程序当天最后访问记录.sql

--- 杨巳煜-TOP10小程序当天最后访问记录.sql
+++ 杨巳煜-TOP10小程序当天最后访问记录.sql
@@ -1,3 +1,13 @@
+--表A结构为：
+CREATE TABLE user_visit_log(
+     ds STRING COMMENT '日期 分区字段',
+     user_id BIGINT COMMENT 'Uin',
+     visit_time BIGINT COMMENT '访问时间戳 单位 s',
+     scene BIGINT COMMENT '进入应用号场景 ',
+     sessionid STRING COMMENT '单次访问会话 id',
+     appid STRING COMMENT 'appid'
+)
+--用sql获取当天访问人数TOP10的小程序当天最后一个会话访问的用户、场景和会话id
 
 --TOP10小程序的最后访问记录
 
